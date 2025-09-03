399d4f5eae6ccbbb431d3be51e3cbf99cc19a626
coopereweb-prog <coopereweb@gmail.com>
2025-09-03 18:58:45 -0300

    correção de deploy 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx';
-import LoginPage from './pages/LoginPage.jsx'; // Padronizado para PascalCase
+import LoginPage from './pages/loginPage.jsx';
 import AdminPage from './pages/AdminPage.jsx';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
 
