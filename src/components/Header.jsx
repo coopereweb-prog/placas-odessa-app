5b2411a1ce5df211181eb577f384507203678592
coopereweb-prog <coopereweb@gmail.com>
2025-09-04 11:44:42 -0300

    ajustes para rodar local primeiro

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={{ 
+      padding: '20px', 
+      backgroundColor: '#18181b', 
+      color: 'white', 
+      textAlign: 'center', 
+      borderBottom: '1px solid #27272a' 
+    }}>
+      <h1>Placas Nova Odessa</h1>
+      <p>Bem-vindo ao nosso site</p>
+    </header>
+  );
+};
+
+export default Header;
