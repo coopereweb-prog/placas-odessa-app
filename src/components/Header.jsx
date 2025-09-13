36e0798ac4e202e98ae44f33bf4d2c6796018bb5
coopereweb-prog <coopereweb@gmail.com>
2025-09-12 22:00:01 -0300

    Volta do H1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
         <div className="flex items-center justify-between">
           <div>
-            <h1 className="text-3xl font-bold text-gray-800">Logradouros</h1>
+            <h1 className="text-3xl font-bold text-gray-800">Placas Nova Odessa</h1>
             <p className="text-gray-500 mt-1">Sistema de Gestão de Placas de Logradouros</p>
           </div>
         </div>
